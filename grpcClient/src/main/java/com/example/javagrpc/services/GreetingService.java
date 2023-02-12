3ff210809619d9d58fca0b736f991539679b072d
siddharthmarvania <s.j.marvania1512@gmail.com>
2023-02-12 17:58:02 +0530

    implement grpc in java

---
+++ grpcClient/src/main/java/com/example/javagrpc/services/GreetingService.java
@@ -0,0 +1,25 @@
+package com.example.javagrpc.services;
+
+import com.example.javagrpc.GreetingRequest;
+import com.example.javagrpc.GreetingResponse;
+import com.example.javagrpc.GreetingServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class GreetingService extends GreetingServiceGrpc.GreetingServiceImplBase {
+
+    @Override
+    public void greeting(GreetingRequest request, StreamObserver<GreetingResponse> responseObserver) {
+        String message = request.getMessage();
+        System.out.println("Received Message: " + message);
+
+
+        GreetingResponse greetingResponse = GreetingResponse.newBuilder()
+                .setMessage("Received your " + message + ". Hello From Server. ")
+                .build();
+
+        responseObserver.onNext(greetingResponse);
+        responseObserver.onCompleted();
+    }
+}
